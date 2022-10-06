c938e1c1fc4492fc67053eb89f0a4bf4ac49cfb0
trongphuong456 <phuongtrongnguyen423@gmail.com>
2022-10-06 08:01:35 +0700

    phuong-code

--- src/components/Layouts/Header/style.js
+++ src/components/Layouts/Header/style.js
@@ -141,3 +141,27 @@ export const HeaderCart = styled.div`
     display: block;
   }
 `;
+
+export const DropDownMenuWrapperTH = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+export const DropDownMenuContainerTH = styled.div`
+  position: absolute;
+  top: 59px;
+  display: inline-block;
+  width: 500px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  background-color: #fff;
+  z-index: 1;
+`;
+
+export const DropDownMenuContentTH = styled.div`
+  display: flex;
+  flex-direction: row;
+  & div {
+    display: flex;
+    flex-direction: column;
+  }
+`;
