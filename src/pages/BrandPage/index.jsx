f2c240315b8117703444d75b0105d6fa4dd0f1f6
trongphuong456 <phuongtrongnguyen423@gmail.com>
2022-10-07 09:58:32 +0700

    brand page

--- src/pages/BrandPage/index.jsx
+++ src/pages/BrandPage/index.jsx
@@ -1,17 +1,213 @@
-import * as S from "./styles";
+import { useNavigate } from "react-router-dom";
+
+import { Select, Pagination, Row, Col } from "antd";
+import { TreeSelect } from "antd";
+import { useState } from "react";
+
+import menProducts from "../../assets/fakedata/products/men";
+
+import ScrollTopButton from "../../components/ScrollTopButton";
 
 import Footer from "../../components/Layouts/Footer";
 import Header from "../../components/Layouts/Header";
+import * as S from "./styles";
+
+const { Option } = Select;
+const { SHOW_PARENT } = TreeSelect;
+
+const treeData = [
+  {
+    title: "Casio",
+    value: "0-0",
+    key: "0-0",
+  },
+  {
+    title: "Orient",
+    value: "0-1",
+    key: "0-1",
+  },
+  {
+    title: "Citizen",
+    value: "0-2",
+    key: "0-2",
+  },
+  {
+    title: "Anne Klein",
+    value: "0-3",
+    key: "0-3",
+  },
+  {
+    title: "Mathey-Tissot",
+    value: "0-4",
+    key: "0-4",
+  },
+];
 
 const BrandPage = () => {
+  const navigate = useNavigate();
+  const [value, setValue] = useState([]);
+
+  const onChange = (newValue) => {
+    setValue(newValue);
+  };
+
+  const tProps = {
+    treeData,
+    value,
+    onChange,
+    treeCheckable: true,
+    showCheckedStrategy: SHOW_PARENT,
+    placeholder: "Các thương hiệu nổi tiếng",
+    style: {
+      width: "100%",
+    },
+  };
+
+  const renderProducts = () => {
+    return menProducts.map((item) => {
+      return (
+        <Col
+          key={item.id}
+          xl={4}
+          md={6}
+          sm={8}
+          xm={12}
+          onClick={() => navigate(`/san-pham/${item.id}`)}
+        >
+          <S.ProductItem>
+            <img src={item.image} alt="item" />
+            <h2>{item.name}</h2>
+            <p>{item.price.toLocaleString()}đ</p>
+            <p>{item.category}</p>
+          </S.ProductItem>
+        </Col>
+      );
+    });
+  };
   return (
     <>
       <Header />
-      <S.BrandPageWrapper>
-        <S.BrandPageContainer>
-          <div></div>
-        </S.BrandPageContainer>
-      </S.BrandPageWrapper>
+      <main>
+        <S.BrandPageWrapper>
+          <div className="brand-container">
+            <div className="brand-item">
+              <img
+                alt="Casio logo"
+                src="https://cdn.tgdd.vn/Brand/1/Casio7264-b_39.jpg"
+              />
+            </div>
+            <div className="brand-item">
+              <img
+                alt="Orient logo"
+                src="https://cdn.tgdd.vn/Brand/1/ORIENTl-220x48.jpg"
+              />
+            </div>
+            <div className="brand-item">
+              <img
+                alt="Citizen logo"
+                src="https://cdn.tgdd.vn/Brand/1/Citizen7264-b_41.jpg"
+              />
+            </div>
+            <div className="brand-item">
+              <img
+                alt="Anne logo"
+                src="https://cdn.tgdd.vn/Brand/1/ANNEKLEINl-220x48.jpg"
+              />
+            </div>
+            <div className="brand-item">
+              <img
+                alt="Tissot logo"
+                src="https://cdn.tgdd.vn/Brand/1/MATHEYTISSOTl-220x48.jpg"
+              />
+            </div>
+          </div>
+
+          <S.TextBrandWrapper>
+            <p className="text-content">
+              "Một sản phẩm có thể lỗi thời nhanh chóng, nhưng một thương hiệu
+              thành công sống mãi với thời gian"
+            </p>
+          </S.TextBrandWrapper>
+
+          <S.SearchBrandWrapper>
+            <input type="text" placeholder="Tìm tên thương hiệu" />
+            <button>
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+          </S.SearchBrandWrapper>
+
+          <S.BrandFilterWrapper>
+            <span>Bộ lọc: </span>
+            <TreeSelect {...tProps} style={{ width: 200, marginLeft: "8px" }} />
+            <Select
+              allowClear
+              placeholder="Giá"
+              style={{
+                width: 120,
+                marginLeft: "8px",
+              }}
+            >
+              <Option value="low-high">Thấp - Cao</Option>
+              <Option value="high-low">Cao - Thấp</Option>
+            </Select>
+            <Select
+              allowClear
+              placeholder="Loại máy"
+              style={{
+                width: 160,
+                marginLeft: "8px",
+              }}
+            >
+              <Option value="automatic">Cơ tự động(automatic)</Option>
+              <Option value="pin">Dùng pin</Option>
+            </Select>
+            <Select
+              allowClear
+              placeholder="Đường kính mặt"
+              style={{
+                width: 160,
+                marginLeft: "8px",
+              }}
+            >
+              <Option value="36">Dưới 36mm</Option>
+              <Option value="36-40">Từ 36mm - 40mm</Option>
+              <Option value="40-44">Từ 40mm - 44mm</Option>
+              <Option value="44">Trên 44mm</Option>
+            </Select>
+            <Select
+              allowClear
+              placeholder="Chất liệu kính"
+              style={{
+                width: 160,
+                marginLeft: "8px",
+              }}
+            >
+              <Option value="mineral">Kính khoáng</Option>
+              <Option value="sapphire">Sapphire</Option>
+            </Select>
+          </S.BrandFilterWrapper>
+
+          <S.ProductsWrapper>
+            <Row gutter={[4, 4]}>
+              {renderProducts()}
+              {renderProducts()}
+              {renderProducts()}
+              {renderProducts()}
+            </Row>
+          </S.ProductsWrapper>
+
+          <Pagination
+            defaultCurrent={1}
+            total={50}
+            style={{
+              padding: "24px 0",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          />
+        </S.BrandPageWrapper>
+      </main>
+      <ScrollTopButton />
       <Footer />
     </>
   );
--- src/pages/BrandPage/styles.js
+++ src/pages/BrandPage/styles.js
@@ -1,14 +1,122 @@
 import styled from "styled-components";
 
-export const BrandPageContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-  align-items: flex-start;
-  width: 80%;
-  margin: 120px auto 0;
+export const BrandPageWrapper = styled.div`
+  width: 1200px;
+  margin: calc(var(--header-height) + 20px) auto 20px;
+  background-color: #fff;
+
+  .brand-container {
+    display: flex;
+    width: 100%;
+    padding: 12px;
+    .brand-item {
+      display: flex;
+      justify-content: center;
+      width: 20%;
+      border: 1px solid #ddd;
+      border-collapse: collapse;
+      transition: all 0.2s ease;
+      cursor: pointer;
+
+      &:hover {
+        border: 1px solid #ffd591;
+        box-shadow: var(--box-shadow);
+        z-index: 99;
+      }
+    }
+  }
 `;
 
-export const BrandPageWrapper = styled.div`
-  height: 700px;
+export const TextBrandWrapper = styled.div`
+  width: 70%;
+  margin: 10px auto 0;
+
+  .text-content {
+    text-align: center;
+    font-size: 20px;
+    font-family: "Times New Roman", Times, serif;
+  }
+`;
+
+export const SearchBrandWrapper = styled.div`
+  flex: 1;
+  display: flex;
+  margin-left: 12px;
+  align-items: center;
+
+  & input {
+    height: 36px;
+    width: 400px;
+    padding: 8px;
+    margin-right: 4px;
+    outline: none;
+    border: none;
+    border-radius: 20px;
+    background-color: #f0f0f0;
+    transition: all 0.2s ease;
+
+    &:focus,
+    &:active {
+      border: none;
+      background-color: #fff;
+
+      outline: 2px solid #d9d9d9;
+      box-shadow: var(--box-shadow);
+    }
+  }
+
+  & button {
+    padding: 4px;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+
+    & i {
+      font-size: 18px;
+    }
+  }
+`;
+
+export const BrandFilterWrapper = styled.div`
+  margin-left: 2px;
+  padding: 12px;
+  border-bottom: 1px solid #f5f5f5;
+`;
+
+export const ProductsWrapper = styled.div`
+  padding: 12px;
+`;
+
+export const ProductItem = styled.div`
+  flex: 1;
+  height: 100%;
+  padding: 8px;
+  color: var(--dark-text-color);
+  text-align: center;
+  border: 1px solid #f5f5f5;
+  border-radius: 2px;
+  overflow: hidden;
+  cursor: pointer;
+
+  & h2 {
+    font-size: 0.9rem;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+  }
+
+  & img {
+    top: 0;
+    width: 100%;
+    transition: all 0.2s ease;
+  }
+
+  &:hover {
+    box-shadow: 0 0 8px #ccc;
+
+    & img {
+      transform: translateY(-5px);
+    }
+  }
 `;
